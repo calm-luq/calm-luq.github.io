885562e5114c1b2bd8325ee3aa967095aa271596
calm-luq <1064406746@qq.com>
2022-02-08 15:23:04 +0800

    Site updated: 2022-02-08 15:23:04

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,4 +1,4 @@
-// 注意：live2d_path 参数应使用绝对路径
+// 注意：live2d_path 参数应使用绝对路径,此处定义看板娘路径
 //const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 const live2d_path = "/live2d-widget/";
 
