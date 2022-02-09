56e48bcb3f569f7032fd0b3bba13bc5090570fcc
calm-luq <1064406746@qq.com>
2022-02-09 11:42:53 +0800

    Site updated: 2022-02-09 11:42:53

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
+			//apiPath: "https://live2d.fghrsh.net/api/",该处已经被关闭，可以自行搭建
 			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
